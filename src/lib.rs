12d9e79a286d7f5c21b29599fc5416db8e0756e7
petrostrak <pit.trak@gmail.com>
2022-06-09 20:47:21 +0300

    Refactor code to use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -24,13 +24,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments!")
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did't get a filename"),
+        };
 
         let case_sensitive = env::var("CASE_SENSITIVE").is_err(); // export CASE_SENSITIVE=true
     
@@ -39,15 +44,10 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
